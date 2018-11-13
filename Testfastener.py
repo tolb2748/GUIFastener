04c491ca31fa4b21a2b0d996e50eb07966589ff2
Brandon Tolbert <brandon.t.tolbert-1@ou.edu>
2018-11-12 20:07:13 -0600

    Set Tab labels, added safety factor variable, set safety Margin so that the user cannot edit it

--- Testfastener.py
+++ Testfastener.py
@@ -14,18 +14,29 @@ from OOPFastenerCalcs import FastenerCalcs
 class myWindwo(QMainWindow):
     def __init__(self):
         super().__init__();
+        #Loads the interface
         loadUi("Fastener_StrengthCriteria_110918.ui", self);
         
+        #Rename Tabs
+        self.tabFastenerWidget.setTabText(0, "TensionAndShear");
+        self.tabFastenerWidget.setTabText(1, "ShearTearOut");
+        self.tabFastenerWidget.setTabText(2, "BoltBearing");
+        self.safetyFactor.setText("1.15");
+        self.safetyMargin.setReadOnly(True);
+        
+        #button click event
         self.calculateButton.clicked.connect(self.calculateSafetyMargin);
-    
+        
+    #handler
     def calculateSafetyMargin(self):
         yieldStress = float(self.yieldStress.text());
         nominalDiameter = float(self.nominalDiameter.text());
         shearOne = float(self.shearOne.text());
         shearTwo = float(self.shearTwo.text());
         axial = float(self.axial.text());
+        safetyFactor =float(self.safetyFactor.text());
    
-        x = FastenerCalcs(yieldStress, nominalDiameter, shearOne, shearTwo, axial);
+        x = FastenerCalcs(yieldStress, nominalDiameter, shearOne, shearTwo, axial, safetyFactor);
         self.safetyMargin.setText(str(round(x.marginOfSafety(),2))); 
     
 
